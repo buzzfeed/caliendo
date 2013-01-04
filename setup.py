714c3c1a9b5f24800bbd8101e9c514aad0ee2f89
Andrew Kelleher <akellehe@gmail.com>
2013-01-04 16:50:00 -0500

    Incremented version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
     name='caliendo',
-    version='v0.2.0',
+    version='v0.2.1',
     packages=find_packages(),
     author='Andrew Kelleher',
     author_email='andrew.kelleher@buzzfeed.com',
