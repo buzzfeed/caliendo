b0aa5096997ac1070f1ee674b0dc5f84c40b262b
Jeff <jeff.revesz@buzzfeed.com>
2013-12-02 17:49:26 -0500

    bumped version to v2.0.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
     name='caliendo',
-    version='v2.0.4',
+    version='v2.0.5',
     packages=find_packages(),
     author='Andrew Kelleher',
     author_email='andrew.kelleher@buzzfeed.com',
