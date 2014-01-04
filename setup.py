2c8f49c10651ebf845618a4a2acc345fd03d19ea
Andrew Kelleher <akellehe@gmail.com>
2014-01-04 17:11:15 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-"""Setup script for the Caliendo Facade"""
-
 __author__ = "andrew.kelleher@buzzfeed.com (Andrew Kelleher)"
 
 try:
@@ -11,17 +9,22 @@ except ImportError:
 
 setup(
     name='caliendo',
-    version='2.0.8',
+    version='2.1.0',
     packages=find_packages(),
     author='Andrew Kelleher',
     author_email='andrew.kelleher@buzzfeed.com',
     description='Makes mocking services for tests simpler.',
-    long_description='Allows patching and caching of target methods with VERY little effort.',
+    long_description=read('README.md'),
     test_suite='test.caliendo_test',
     install_requires=[
         'mock==1.0.0',
         'dill==0.2b1'
     ],
     url='http://www.github.com/buzzfeed/caliendo',
-    download_url='https://github.com/buzzfeed/caliendo/archive/2.0.8.tar.gz'
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+    ],
+    keywords="test testing mock mocking api service cache caching integration unit",
 )
