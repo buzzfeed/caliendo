796f975b2d56d0305f3e2d7e9d8db8d59d228925
Andrew Kelleher <akellehe@gmail.com>
2014-04-06 10:29:26 -0400

    Bumped version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='caliendo',
-    version='2.1.6',
+    version='2.1.7',
     packages=find_packages(),
     author='Andrew Kelleher',
     author_email='andrew.kelleher@buzzfeed.com',
